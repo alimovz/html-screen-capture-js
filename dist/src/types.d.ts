34998d8ec132d72529fa0f610478b2968d387863
alimovz <alimovz@gmail.com>
2025-01-12 18:09:35 -0700

    compiled to dist

---
+++ dist/src/types.d.ts
@@ -0,0 +1,26 @@
+export declare enum OutputType {
+    OBJECT = "object",
+    STRING = "string"
+}
+export declare enum LogLevel {
+    DEBUG = "debug",
+    INFO = "info",
+    WARN = "warn",
+    ERROR = "error",
+    FATAL = "fatal",
+    OFF = "off"
+}
+export interface Options {
+    rulesToAddToDocStyle?: string[];
+    cssSelectorsOfIgnoredElements: string[];
+    tagsOfSkippedElementsForChildTreeCssHandling?: string[];
+    attrKeyForSavingElementOrigClass?: string;
+    attrKeyForSavingElementOrigStyle?: string;
+    prefixForNewGeneratedClasses?: string;
+    prefixForNewGeneratedPseudoClasses?: string;
+    imageFormatForDataUrl?: string;
+    imageQualityForDataUrl?: number;
+    logLevel?: LogLevel;
+}
+export type CaptureOutput = HTMLElement | string | null;
+export type CaptureFunction = (outputType?: OutputType, htmlDocument?: HTMLDocument, options?: Options) => CaptureOutput;
